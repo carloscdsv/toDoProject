0a3c484afbc0b9645a79b37e79496994b3a58135
carlos <carloscatalandasilva@gmail.com>
2025-05-21 19:25:00 +0200

    fix: added domain to ALLOWED_HOSTS

--- toDoProject/settings.py
+++ toDoProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#jl7lbq7@)&k5+moo#3ge1o#^4ph8+r*z#y%-!kvy2i=d+lel%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
